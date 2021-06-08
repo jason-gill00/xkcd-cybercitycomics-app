83f3d7f5ddfa76a612e5189994b43a2d71753808
jason-gill00 <59940873+jason-gill00@users.noreply.github.com>
2021-06-08 10:39:31 -0400

    Bug Fixes

--- app.js
+++ app.js
@@ -10,10 +10,27 @@ const PORT = process.env.PORT || 5000;
 const app = express()
 
 //Middleware
+
+const whitelist = ['http://localhost:5000', 'http://localhost:8080', 'https://xkcdstratus360cybercitycomics.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
 app.use(cors());
 app.use(express.json())
 
 app.use('/api', apiRoutes)
+app.set('port', (process.env.PORT));
 
 if (process.env.NODE_ENV === 'production'){
     // app.use(express.static('../cyber-city-comics/build'));
@@ -30,7 +47,6 @@ if (process.env.NODE_ENV === 'production'){
     })
 } 
 
-app.set('port', (process.env.PORT));
 
 app.listen(PORT, () => {
     console.log(`Running on ${PORT}...`)
