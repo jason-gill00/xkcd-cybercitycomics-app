44f753cc4f9bd256d1e92bced9863d4b8f5e6d7f
jason-gill00 <59940873+jason-gill00@users.noreply.github.com>
2021-06-08 23:39:17 -0400

    Bug Fix

--- app.js
+++ app.js
@@ -11,25 +11,10 @@ const app = express()
 
 //Middleware
 
-const whitelist = ['http://localhost:5000', 'http://localhost:8080', 'https://xkcdstratus360cybercitycomics.herokuapp.com']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json())
 
-app.set('port', (process.env.PORT));
 app.use('/api', apiRoutes)
 
 if (process.env.NODE_ENV === 'production'){
