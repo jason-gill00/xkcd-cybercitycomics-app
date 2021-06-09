4c7cdb16dc6832c70f57b514b4e6666fc60a8f87
jason-gill00 <59940873+jason-gill00@users.noreply.github.com>
2021-06-08 23:27:54 -0400

    Bug Fix

--- app.js
+++ app.js
@@ -11,7 +11,23 @@ const app = express()
 
 //Middleware
 
-app.use(cors());
+/* The following code is to fix a Heroku bug perventing API Requests when deployed */
+const whitelist = ['http://localhost:5000', 'http://localhost:8080', 'https://xkcdstratus360cybercitycomics.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+/* */
+
 app.use(express.json())
 
 app.use('/api', apiRoutes)
